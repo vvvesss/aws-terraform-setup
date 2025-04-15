22c923eda40c4732026c1f39d02a56516e0cce72
Vesselin Yanev <veselin.yanev@gmail.com>
2025-04-15 16:04:43 +0300

    backend init state

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
 #(initially commented)
-terraform {
+/*terraform {
   backend "s3" {
-    bucket         = "ves-tf-state-bucket"  # Replace with your bucket name
+    bucket         = "your-unique-tf-state-replica-bucket""  # Replace with your bucket name
     key            = "terraform/state/terraform.tfstate"
     region         = "eu-central-1"       # Replace with your region
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
-
+*/
